62551ecc2a582f7c73baab5b57add7745268e9e7
mcpeer <>
2018-12-22 21:18:21 +0100

    added province pages

--- django_brederland/core/views.py
+++ django_brederland/core/views.py
@@ -11,9 +11,32 @@ class IndexView(generic.ListView):
     context_object_name = 'top_municipality_list'
 
     def get_queryset(self):
-            """
-            Return the existing municipalities
+        """
+        Return the existing municipalities
 
-            note: order_by('?') is known to be slow
-            """
-            return Municipality.objects.order_by('?')[:4]
+        note: order_by('?') is known to be slow
+        """
+        return Municipality.objects.order_by('?')[:4]
+
+class ProvinceView(generic.ListView):
+    template_name = 'core/provincie.html'
+    context_object_name = 'province_municipality_list'
+    
+    def get_queryset(self):
+        """
+        Fetches province name 
+        Parses it in correct format (e.g. noord-brabant --> Noord-Brabant)
+        Returns the municipalities that belong to the 
+        province 'province_label'
+        """
+        province_label = self.kwargs['province']
+        split = province_label.split('-')
+        province_label_fin = None
+
+        for word in split:
+            if province_label_fin is None:
+                province_label_fin = word.capitalize()
+            else: 
+                province_label_fin = province_label_fin + '-' + word.capitalize()
+
+        return Municipality.objects.filter(province=province_label_fin).order_by('label')
