e09b687ec57f4b9f5802f326590599efd1abe9c5
mcpeer <>
2018-12-29 09:41:33 +0100

    added first working login logout flow

--- django_brederland/django_brederland/settings.py
+++ django_brederland/django_brederland/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_brederland.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# redirect url
+LOGIN_REDIRECT_URL = 'core:index'
+LOGOUT_REDIRECT_URL = 'core:index'
